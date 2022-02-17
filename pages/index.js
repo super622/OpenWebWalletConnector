1aadc1cf269666aeb8ae15b44bd2162cb209d4c1
superhorse622 <masonrose622@gmail.com>
2022-02-17 18:46:33 +0000

    Updated index.js, with execute func

--- pages/index.js
+++ pages/index.js
@@ -36,4 +36,19 @@ export default function Home() {
       }
     }
   }
+
+  async function execute() {
+    if (active) {
+      const signer = provider.getSigner();
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store(42);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Please install MetaMask");
+    }
+  }
 }
